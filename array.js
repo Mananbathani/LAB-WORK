7bf5824d3e4e8a8f8fc70e29b0d05fc9873aba27
Mananbathani <mananbathani117@gmail.com>
2026-06-23 11:05:29 +0530

    22/06/2026

---
+++ array.js
@@ -0,0 +1,109 @@
+// ----------QUESTION:-1 ----------
+
+
+        // var Arr = [1, 2, 3];
+        // console.log(Arr);
+
+        // Arr.push(4);
+        // console.log("After Push",Arr);
+
+        // Arr.pop();
+        // console.log("After  Pop:-",Arr);
+
+
+// ----------QUESTION:-2 ----------
+ 
+
+        // var Arr = [];
+        // console.log(Arr);
+
+        // Arr.push(1, 2, 3);
+        // console.log(Arr);
+
+        // Arr.shift();
+        // console.log(Arr);
+
+
+// ----------QUESTION:-3 ----------
+
+
+        // var Arr = [];
+        // console.log();
+
+        // Arr.push(1, 2, 3);
+        // console.log(Arr);
+
+        // Arr.pop();
+        // console.log(Arr);
+
+
+// ----------QUESTION:-4 ----------
+
+    
+        // var Arr = ['b', 'c'];
+        // console.log();
+
+        // Arr.unshift("a");  
+        // console.log(); 
+
+        // Arr.shift();
+        // console.log();
+
+
+// ---------- QUESTION:-5 ----------
+
+
+        // var Arr = [11, 22, 33, 44, 55];
+        // console.log("Origanal Array:-", Arr);
+        // Arr.reverse();
+        // console.log("Revers Array:-", Arr);
+
+
+// ----------QUESTION:-6 ----------
+
+
+
+        // var Arr = ["John", "Jane", "Doe"];
+        // console.log(Arr);
+
+        // Arr.push("Smith");
+        // console.log("After Pushing:-", Arr);
+
+        // Arr.pop();
+        // console.log("After Poping:-", Arr);
+
+        // Arr.unshift("Alice");
+        // console.log("After Unsifting:-", Arr)
+
+        // Arr.shift();
+        // console.log("After Shifting:-", Arr);
+
+  
+// ----------QUESTION:-7 ----------
+
+
+
+        // var Arr = [`Apple`, `Orange`, `Banana`];
+        // console.log(Arr);
+
+        // Arr.push(`Grapes`);
+        // console.log("After Pushing:-", Arr);
+
+        // Arr.pop();
+        // console.log("After Poping:-", Arr);
+
+        // Arr.unshift(`Pear`);
+        // console.log("After Unshifting:-", Arr)
+
+        // Arr.shift();
+        // console.log("After Shifting:-", Arr);
+
+
+// ----------QUESTION:-8 ----------
+
+
+
+        // var Arr = [1, 2, 3, 4, 5];
+        // console.log();
+        // Arr.splice(0, 1);
+        // console.log(Arr);
